472ad776b0c0ca84c1f5220177d1b1632d0d501d
Ishan Apte <ishanapte@Ishans-MacBook-Air.local>
2024-09-15 20:08:34 -0400

    menu/close button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,14 +67,14 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+            className={`w-[28px] h-[28px] object-contain ${toggle ? "text-white" : "text-black"}`}
             onClick={() => setToggle(!toggle)}
           />
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 black absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
